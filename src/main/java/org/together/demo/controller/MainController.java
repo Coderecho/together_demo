058fca1f99fb2a463e7f5471471069cd0d0c63ed
huwei <whuhw2008@hotmail.com>
2017-03-08 18:55:52 +0800

    init

---
+++ src/main/java/org/together/demo/controller/MainController.java
@@ -0,0 +1,17 @@
+package org.together.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by daily on 2017/3/8.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/test.do",method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
